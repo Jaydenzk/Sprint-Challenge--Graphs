28935d624b149c099c9e3f305f2f6f85685f4669
Jaydenzk <joowan88@gmail.com>
2020-06-12 08:05:24 -0700

    Done

--- adv.py
+++ adv.py
@@ -30,6 +30,107 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+def pick_unexplored(room):
+    """Using a fixed order, return a room's first unexplored direction"""
+    directions_sequence = ['n', 's', 'e', 'w']
+    random.shuffle(directions_sequence)
+    for direction in directions_sequence:
+        explored = room.get(direction)
+        if explored == '?':
+            return direction
+    return None
+
+
+def find_unexplored(room, graph):
+    """Look for closest unexplored exit and return a path to it"""
+    # Create an empty queue
+    q = []
+    # Add a path to the room_id to the queue
+    q.append([room])
+    # Create an empty set to store visited rooms
+    visited = set()
+    # While the queue is not empty...
+    while q:
+        # Dequeue the first path
+        path = q.pop(0)
+        # grab the last room from the path
+        room = path[-1]
+        # if last room has an unexplored exit, return the path
+        if pick_unexplored(graph[room]):
+            rewind = []
+            for i in range(1, len(path)):
+                for room_direction, room_id in graph[path[i-1]].items():
+                    if room_id == path[i]:
+                        rewind.append(room_direction)
+                        break
+            return rewind
+        # If it has not been visited...
+        if room not in visited:
+            # Mark it as visited
+            visited.add(room)
+            # Then add a path to all unvisited rooms to the back of the queue
+            for next_room in graph[room].values():
+                if next_room not in visited:
+                    q.append(path + [next_room])
+
+    return None
+
+
+def explore_world(player, traversal_path):
+    reverse = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
+    # add first room to graph and initialize exits
+    graph = {player.current_room.id: {}}
+    for exits in player.current_room.get_exits():
+        graph[player.current_room.id][exits] = '?'
+    # enter a room in an unexplored direction
+    direction = pick_unexplored(graph[player.current_room.id])
+    if direction is None:
+        print("Help, I am trapped in a room with no exits!")
+        return -1
+
+    prev_room = player.current_room
+    player.travel(direction)
+    traversal_path.append(direction)
+
+    while True:
+        # if new room, add to graph and initialize exits
+        if player.current_room.id not in graph:
+            graph[player.current_room.id] = {}
+            for exits in player.current_room.get_exits():
+                graph[player.current_room.id][exits] = '?'
+        # if entered from unexplored direction, make connections between room and previous room
+        if graph[player.current_room.id][reverse[direction]] == '?':
+            graph[player.current_room.id][reverse[direction]] = prev_room.id
+            graph[prev_room.id][direction] = player.current_room.id
+        # if there is an unexplored exit:
+        direction = pick_unexplored(graph[player.current_room.id])
+        if direction:
+            # enter room in that direction
+            prev_room = player.current_room
+            player.travel(direction)
+            # add step to traversal_path
+            traversal_path.append(direction)
+        # else there is not an unexplored exit:
+        else:
+            # map a path to the closest unexplored exit
+            return_path = find_unexplored(player.current_room.id, graph)
+            # if there are no unexplored exits, traversal is complete
+            if return_path is None:
+                break
+            # walk path while adding to traversal_path
+            while return_path:
+                direction = return_path.pop(0)
+                player.travel(direction)
+                traversal_path.append(direction)
+
+    return graph
+
+
+random.seed(20631)
+explore_world(player, traversal_path)
+
+
+
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
